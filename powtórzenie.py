20ee2cce41feb9a4c2cedadde779b594d9ee49f9
Uczeń <Ucze@ZSO1_1>
2023-10-18 12:37:25 +0200

    sfhtjf

--- powtórzenie.py
+++ powtórzenie.py
@@ -1,17 +1,29 @@
 # Co wypisuje print? - cokolwiek
 
 #a zmienna
- x = 'teodor'
- print(x)_
+x = 'teodor'
+print(x)
 
- #b) tekst
- print('informatyka')
+#b) tekst
+print('informatyka')
 
- #c) wyniki działań
- print(2 * (6 + 3) / 7)
+#c) wyniki działań
+print(2 * (6 + 3) / 7)
 
- #d) zaawansowane typy
- lista1 = {'x', 6, 'słoń', 12, 9.6)
+#d) zaawansowane typy
+lista1 = ['x', 6, 'słoń', 12, 9.6]
 
- #e) coś
- print(True)
+#e) coś
+print(True)
+
+#f) komunikaty złożone
+imie = 'Janusz'
+wiek = 45
+nazwisko = 'Kowal'
+
+print(imie + '\t' + nazwisko + '\tma\t' +str(wiek) + 'lat')
+
+print('{0} {1}\nma lat\n{2}'.format( imie, nazwisko, wiek))
+
+#\n-przejście do nowej linii
+#\t-tabulator
