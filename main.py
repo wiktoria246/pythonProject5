1af207caf3da6a81a7e01dbfa91e5230237cabea
Uczeń <Ucze@ZSO1_1>
2023-10-18 12:01:20 +0200

    sfhtjf

---
+++ main.py
@@ -0,0 +1,25 @@
+#1. Wczytywanie danych od uzytkownika:
+
+'''x = input('podaj liczbe')
+
+#a) zatrzymanie programu:
+#b)wyswietla komunikat 'podaj liczbe'
+#c)czeka na to co podał użytkownik
+#d) to co poda uzytkownik trafi do zmniennej x -jako TEKST
+
+print(type(x)) #pokazujemy jakiego typu jest dana w zmiennej x'''
+
+'''#2. Jak wczytać inna daną niż tekst
+
+y = input('podaj liczbe')
+z = int(y) # zminenna y zosataje zmienona na typ int i zapisana w zmiennej z
+
+print(type(y)) #y to tekst
+print(type(z)) #z to liczba'''
+
+'''#3. Jak wczytac inna dana niz rtekst - wersja krótsza
+y = int(input('podaj liczbe'))'''
+
+#4. Jak wczytac inna dana niz rtekst - przykład z float
+y = float(input('podaj liczbe'))
+print(type(y))
---
+++ powtórzenie.py
@@ -0,0 +1,17 @@
+# Co wypisuje print? - cokolwiek
+
+#a zmienna
+ x = 'teodor'
+ print(x)_
+
+ #b) tekst
+ print('informatyka')
+
+ #c) wyniki działań
+ print(2 * (6 + 3) / 7)
+
+ #d) zaawansowane typy
+ lista1 = {'x', 6, 'słoń', 12, 9.6)
+
+ #e) coś
+ print(True)
