258146f1afd5bf75abe4652e72293d1cea50b411
Uczeń <Ucze@ZSO1_1>
2023-11-06 10:09:40 +0100

    sfhtjf

--- zadanie 12.py
+++ zadanie 12.py
@@ -1,9 +1,12 @@
-a = print('podaj wynik egzaminu pisemnego z jezyka polskiego')
-b = print('podaj wynik egzaminu pisemnego z jezyka obcego')
-c = print('podaj wynik egzaminu dodatkowego')
-d = print('podaj wynik egzaminu usntego z jezyka polskiego')
-e = print('podaj wynik egzaminu usntego z jezyka obcegp')
+a = int(input('podaj wynik egzaminu pisemnego z jezyka polskiego'))
+b = int(input('podaj wynik egzaminu pisemnego z jezyka obcego'))
+c = int(input('podaj wynik egzaminu dodatkowego'))
+d = int(input('podaj wynik egzaminu usntego z jezyka polskiego'))
+e = int(input('podaj wynik egzaminu usntego z jezyka obcegp'))
 
 if a >= 30 and b >= 30 and c >= 30 and d >= 30 and e >= 30:
-    print('zdałeś mature')
-
+    print('zdałeś mature bez amnesii')
+elif a + b + c + d + e / 5 >= 30:
+    print('zdałeś mature z amnesiią')
+else:
+    print('widzimy sie w sierpniu ')
