c25ee96d20dd958a211438089da70add9d08a163
Uczeń <Ucze@ZSO1_1>
2023-11-06 10:29:20 +0100

    sfhtjf

---
+++ pętle.py
@@ -0,0 +1,8 @@
+przedzial1 = list(range(1, 11)) #<1, 11) - przedział liczb całkowitych od 1 do 10
+print(przedzial1)
+
+przedzial2 = list(range(11)) #<0, 11) - przedział liczb całkowitych od 0 do 10
+print(przedzial2)
+
+przedzial3 = list(range(-60, 120, 6)) #<=60, 120) - przedzial liczb całkowitycfh od -60 do 120
+print(przedzial3)
