260d261ef244976bc6dca5ae1f95573c66d50065
Uczeń <Ucze@ZSO1_1>
2023-11-08 12:39:38 +0100

    sfhtjf

---
+++ zadanie16.py
@@ -0,0 +1,19 @@
+#a
+'''y = 11
+z = 13
+for x in range(3, 31, 3):
+    print('{}\t{}\t{}'.format(x, y, z))
+    y = y + 11
+    z= z + 13'''
+#2
+'''x = 3
+y =11
+z = 13
+for i in range(10)
+    print('{}\t{}\t{}'.format(x, y, z))
+    y = y + 11
+    z = z + 13
+    x = x + 3'''
+
+#3 sposób
+for x, y, z in zip(list(range(3, 31, 3)) 
