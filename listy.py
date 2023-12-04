67219c3940fe09f9e8af7c618ff7c28544abf5e8
Uczeń <Ucze@ZSO1_1>
2023-12-04 10:30:01 +0100

    listy.py

--- listy.py
+++ listy.py
@@ -1,7 +1,7 @@
 #1. umieszczenie elementow na liście
 #2. recznie wpisane
 
-lista1 = ['kot', 'pies', 'kapibara', 'orzeł']
+lista1 = ['kot', 1, 3.1, [4, 4, 3], 'pies', 'kapibara', 'orzeł']
 
 #3. powielanie listy
 
@@ -11,26 +11,28 @@ print(lista2)
 lista3 = [1, 2, 3] * 4
 print(lista3)
 
-#4. wrzucanie danych element po elemecie
+#4. wrzucanie danych element po elemecie(funkcja append)
 
 lista4 = []
-for i in range(1, 101):
-    lista4.append(i)
+n = int(input('ile bedzie liczb?'))
+for i in range(n):
+    liczba = float(input('podaj kolejna liczbe'))
+    lista4.append(i)#wsatwiamy liczbe na koncu
 print(lista4)
 
-#5. wstawiane elementów na dowolnej pozycji
+#5. wstawiane elementów na dowolnej pozycji(funkcja insert)
 lista5 = [4, 1, 7, 5]
-lista5.insert(1, 56)
+lista5.insert(1, 56)#na pozycji 1 wstawiamy liiczbe 56
 print(lista5)
 
 #6.rozszerzanie listy
 
 lista6 = [5, 1, 5, 9, 3]
 lista7 = [8, 9, 0, 1]
-lista7.extend(lista6)
+lista7.extend(lista6)#dopisanie do listy 3 elementów listy 4
 print(lista7)
 
-#7.konkatynacja list(sklejanie elementów)
+#7.konkatynacja list(sklejanie elementów)- rozszerzenie liczby sposób 2
 
 lista8 = [6, 1, 4, 9]
 lista9 = [-1, 8, 0, 5]
@@ -60,3 +62,17 @@ slowo1_lista = lista(slowo1)
 print(slowo1_lista)
 
 #12.zamiana listy na słowo
+
+
+
+
+
+
+
+#13.jak pobrać pojedyńcze elementy
+#a)korzystahjaćz indeksów dodatnich
+lista14 = [6, 4, 7, 2]
+print(lista14[2])#pobieramy 3 element(numerujemy od 0)
+#b)korzystajac z elementów ujemnych
+lista15[2, 4, 6, 8, 5, 3]
+print(lista15[-2])#pobieramy drugi element od konca(numerujemy od -1 do -długości)
